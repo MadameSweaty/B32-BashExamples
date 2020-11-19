7914212452554b49ddb22989fe240f8fea8d392d
Valerie <val.aube@hotmail.com>
2020-11-19 17:30:39 -0500

    exercice 5 et fait

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+#Sans les ` liste serait egal a "ls" pas la commande ls
+liste=`ls`
+for item in $liste
+do
+	echo $item
+done
+
+touche=o
+
+while test $touche == o
+do
+	read -p "Entrer o pour infini" touche
+done
--- 06-tableau.sh
+++ 06-tableau.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#-a elle declaration de tableau, ici 4 elements Back,To,The et Future
+declare -a nom=(Back To The Future)
+echo ${nom[0]} #Ceci retourne le premier element du tableau
+echo ${nom[1]} #2em element
+
+echo ${#array[@]} #la taille du tableau
+
