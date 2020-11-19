0ca08193fdd2f0eb0f5d63cf32b37027db73d10f
Valerie <val.aube@hotmail.com>
2020-11-19 16:31:57 -0500

    exercice 3 fait

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo 10+10 #affiche 10+10
+echo $((10+10)) #affiche 20
+
+let resultat=10*10
+echo $resultat #affiche 100
+
+#exercice : soustraire deux valeurs passees en parametre
+
+let soustraction=$1-$2
+echo $1 - $2 =  $soustraction
