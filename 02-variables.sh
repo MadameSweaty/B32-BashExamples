aa937fa209dccdb3b358ea568e60d3638588487b
Valerie <val.aube@hotmail.com>
2020-11-19 16:02:49 -0500

    exercice 2 fait

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+prenom=Valerie
+
+echo $prenom 
+
+#les types
+#-i pour les int
+declare -i age=3
+#-r pour lecture seule aka constante
+declare -r AGE=32
+#` permet dinterpreter la valeur dune variable
+`mkdir $prenom`
+
+echo $1 $2 $# $0
+#$1 est le premier mot passer en parametre
+#$2 est le deuxieme
+#$# est le nombre de mots passers en parametres
+#$0 est le nom du fichier
