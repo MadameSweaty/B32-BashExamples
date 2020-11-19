fae23e114d38f85b6699154e02a645fbba5bc8d6
Valerie <val.aube@hotmail.com>
2020-11-19 17:53:39 -0500

    exercice 9 fait

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#tests sur les fichiers
+
+if test -e $1
+then
+	echo "existe"
+else
+	echo "inexistant"
+fi
+
+#Sur les fichiers
+#test -e fichier	vrai si fichier existe
+#test -d fichier	vrai si fichier est repertoire
+#sur les chaines de caracteres
+#test -z chaine		vrai si chaine est de taille nulle
+#test -n chaine 	vrai si chaine n'est pas de taill nulle
