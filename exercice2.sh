56ca2e6e13931d00bb5f0def08e3712e7bb1c869
Valerie <val.aube@hotmail.com>
2020-11-20 10:59:59 -0500

    exercice 1 et 2 terminer

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+afficherMenu() {
+	touche=i
+	while test $touche != q
+	do
+	echo -e "\nAppuyez sur 'd' pour acceder au dictionnaire"
+	echo "Appuyez sur 'q' pour quitter"
+	read -p "Votre choix : " touche
+		if test $touche == "d"; then
+			echo -e "\n"
+			./exercice1.sh
+		elif test $touche == "q";then
+			echo -e "\nAu revoir!"
+		else
+			echo -e "\nTouche incorrect, reessayez..."
+		fi
+		done
+}
+
+afficherMenu
